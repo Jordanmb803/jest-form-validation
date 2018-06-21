b41c51a57c0d2a433e03c2a64dbe0048955acda4
Jordan Buon <jordanmb803@github.com>
2018-06-21 12:34:49 -0600

    lecture completed

---
+++ src/components/Password.test.js
@@ -0,0 +1,11 @@
+let fns = require('../utils/functions');
+
+test('no username fails test', () => {
+    // arrange & act
+    let result = fns.usernameCheck()
+    // assert
+    expect(result).toBeFalsy();
+})
+
+
+
---
+++ src/utils/functions.js
@@ -0,0 +1,13 @@
+module.exports = {
+    usernameCheck(username) {
+        let flag = true
+        if(!username){
+           flag = false 
+           return 
+        } 
+        if(username.length < 6 ) flag = false;
+        if(username.indexOf(' ') != -1) flag = false
+        return flag;
+    }
+}
+
